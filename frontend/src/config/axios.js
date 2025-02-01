5a1e415b8ec16bcc2e526e3712690381b100f924
Kenny Cen <kennycen1976@gmail.com>
2025-02-01 14:20:47 -0500

    cors fix

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
+// Remove any trailing slash from the base URL
+const baseURL = import.meta.env.VITE_API_URL.replace(/\/$/, '')
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: baseURL
 })
 
 export default api 
